be0e26bf2021a7799709ffd1e611edba91ec7fde
Harjot <singh.harjot85@gmail.com>
2025-11-06 15:25:06 -0800

    Improve Prompt

--- packages/client/src/components/chatbot/chatbot.tsx
+++ packages/client/src/components/chatbot/chatbot.tsx
@@ -8,13 +8,21 @@ type ChatResponse = {
     message: string;
 };
 
-const rules = ' Keep the responses brief. ';
+// Keep in mind - Role, Context, Audience, Tone, Output format, Examples (well-formatted, clear, diverse)
+// If the Modal is unsure it can say sorry I don't know, do not guess or make up a response
+
+const groundContext = `You are an expert in travelling. Your role is assist the clients with best knowlegde in a friendly tone.  
+                Keep the reponses very short unless the clients specifically request for a desctiptive response. 
+                Use markdown wherever possible. Use bullet points, bolds and italics whereever possible. 
+                If the requst is too vague, ask 1 or 2 questions to clarify. Do not guess.
+                Here is their query: `;
 
 export const Chatbot = () => {
-    const conversationId = useRef(crypto.randomUUID());
     const [error, setError] = useState('');
     const [messages, setMessages] = useState<Message[]>([]);
     const [isfetching, setIsfetching] = useState(false);
+
+    const conversationId = useRef(crypto.randomUUID());
 
     const onFormSubmit = async (formData: ChatFormData) => {
         try {
@@ -26,8 +34,11 @@ export const Chatbot = () => {
             ]);
             setIsfetching(true);
 
+            const prompt =
+                messages.length === 0 ? groundContext + parsedData : parsedData;
+
             const { data } = await axios.post<ChatResponse>('/api/chat', {
-                prompt: parsedData + rules,
+                prompt: prompt,
                 conversationId: conversationId.current,
             });
 
@@ -52,7 +63,12 @@ export const Chatbot = () => {
     }, [messages]);
 
     return (
-        <div className="px-4 pb-4 flex flex-col flex-1 h-full ">
+        <div className="px-4 pb-4 flex flex-col flex-1 h-full pt-10 bg-gray-950">
+            <div className="flex text-4xl justify-center text-white">
+                Travel GPT
+            </div>
+            <div className="flex self-center border-b-2 border-green-900 mt-5 w-2xl" />
+            <div className="flex self-center border-b-2 border-green-900 mt-1 mb-10 w-2xl" />
             <div className="flex flex-col flex-1 gap-4 overflow-x-auto">
                 {messages?.map((message, index) => (
                     <ChatMessage key={index} message={message} />
